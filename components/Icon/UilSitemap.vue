710e9295f12c80e616dd6ba18effcde8804341f1
Abel Suárez <lebazeraus@gmail.com>
2025-09-24 10:33:37 -0500

    Add

---
+++ components/Icon/UilSitemap.vue
@@ -0,0 +1,10 @@
+<script setup>
+  defineProps({
+    color: { type: String, default: 'var(--color-secondary)' },
+    size: { type: [Number, String], default: 24 }
+  })
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 24 24"><!-- Icon from Unicons by Iconscout - https://github.com/Iconscout/unicons/blob/master/LICENSE --><path :fill="color" d="M22 15h-2v-3a1 1 0 0 0-1-1h-6V9h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2v2H5a1 1 0 0 0-1 1v3H2a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6a1 1 0 0 0-1-1H6v-2h12v2h-2a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6a1 1 0 0 0-1-1M7 17v4H3v-4Zm3-10V3h4v4Zm11 14h-4v-4h4Z"/></svg>
+</template>
