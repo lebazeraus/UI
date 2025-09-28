f71ca7624ff45b97469a23df1b8c68c4ad0807f7
Abel Suárez <lebazeraus@gmail.com>
2025-09-27 20:05:55 -0500

    Add

---
+++ components/Icon/UilSubject.vue
@@ -0,0 +1,10 @@
+<script setup>
+  defineProps({
+    color: { type: String, default: 'var(--color-secondary)' },
+    size: { type: [Number, String], default: 24 }
+  })
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 24 24"><!-- Icon from Unicons by Iconscout - https://github.com/Iconscout/unicons/blob/master/LICENSE --><path :fill="color" d="M13 16H3a1 1 0 0 0 0 2h10a1 1 0 0 0 0-2M3 8h18a1 1 0 0 0 0-2H3a1 1 0 0 0 0 2m18 3H3a1 1 0 0 0 0 2h18a1 1 0 0 0 0-2"/></svg>
+</template>
