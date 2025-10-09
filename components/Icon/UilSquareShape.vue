bf2af7b4fcd1cbea4a32a565feaa4484d02287cc
Abel Suárez <lebazeraus@gmail.com>
2025-10-09 15:57:06 -0500

    Add

---
+++ components/Icon/UilSquareShape.vue
@@ -0,0 +1,10 @@
+<script setup>
+  defineProps({
+    color: { type: String, default: 'var(--color-secondary)' },
+    size: { type: [Number, String], default: 24 }
+  })
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 24 24"><!-- Icon from Unicons by Iconscout - https://github.com/Iconscout/unicons/blob/master/LICENSE --><path :fill="color" d="M17 2H7a5 5 0 0 0-5 5v10a5 5 0 0 0 5 5h10a5 5 0 0 0 5-5V7a5 5 0 0 0-5-5m3 15a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3V7a3 3 0 0 1 3-3h10a3 3 0 0 1 3 3Z" /></svg>
+</template>
