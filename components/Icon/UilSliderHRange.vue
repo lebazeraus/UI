eaad4632e892249b0850c968f74b517c9a258aa1
Abel Suárez <lebazeraus@gmail.com>
2025-12-02 15:24:45 -0500

    Add

---
+++ components/Icon/UilSliderHRange.vue
@@ -0,0 +1,10 @@
+<script setup>
+  defineProps({
+    color: { type: String, default: 'var(--color-secondary)' },
+    size: { type: [Number, String], default: 24 }
+  })
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 24 24"><!-- Icon from Unicons by Iconscout - https://github.com/Iconscout/unicons/blob/master/LICENSE --><path :fill="color" d="M21 11h-1.184a2.982 2.982 0 0 0-5.632 0H9.816a2.982 2.982 0 0 0-5.632 0H3a1 1 0 0 0 0 2h1.184a2.982 2.982 0 0 0 5.632 0h4.368a2.982 2.982 0 0 0 5.632 0H21a1 1 0 0 0 0-2M7 13a1 1 0 1 1 1-1a1 1 0 0 1-1 1m10 0a1 1 0 1 1 1-1a1 1 0 0 1-1 1" /></svg>
+</template>
